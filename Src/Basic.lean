81f1cc27bc4831dabbdcbeca7cb5317a8b26d6a4
Sasha Patotski <alpatots@microsoft.com>
2025-06-27 16:01:59 -0700

    fix build

--- Src/Basic.lean
+++ Src/Basic.lean
@@ -1,8 +1,1 @@
-import Mathlib.Tactic
-import Mathlib.Util.Delaborators
-
-set_option warningAsError false
-
-example (a b c : ℝ) : a * b * c = b * (a * c) := by
- rw [mul_comm a b]
- rw [mul_assoc b a c]
+def hello := "world"
